6648913552f25250bfc04b249808971c8bc77f91
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-07 16:08:16 +0530

    Header seaction: Add js and css file for header section.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     // We will use the BEM convention for naming
     <div className="app">
       <h1>Hello Programmers, Let's build the Amazon store app </h1>
+      {/* Header */}
+      {/* Home */}
     </div>
   );
 }
