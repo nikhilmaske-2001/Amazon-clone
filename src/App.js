d2ddbf8b03e07cfb950b5fd44b5755964128849f
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-07 16:54:54 +0530

    header css: Style the header to be fixed at top.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     // We will use the BEM convention for naming
     <div className="app">
-      <h1>Hello Programmers, Let's build the Amazon store app </h1>
       <Header />
       {/* Header */}
       {/* Home */}
