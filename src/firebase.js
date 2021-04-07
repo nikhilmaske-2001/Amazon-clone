0dbd8d739df7652f7799695a21a3dd51ff6c37f0
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-07 15:30:58 +0530

    amazon-clone: Setup for React js completed.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD7EKIXovtChwx_K_YH6wpGytm5lqvMkHw",
+    authDomain: "clone-ea57e.firebaseapp.com",
+    projectId: "clone-ea57e",
+    storageBucket: "clone-ea57e.appspot.com",
+    messagingSenderId: "93173779250",
+    appId: "1:93173779250:web:9cba5272c8474e3c57252b",
+    measurementId: "G-22F5C268TD"
+  };
