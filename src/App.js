f8791ed04bc1c388ba157f2f8588c123a1e63767
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-07 16:41:51 +0530

    header: Structured the header (top most line). import: Imported the header file in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     // We will use the BEM convention for naming
     <div className="app">
       <h1>Hello Programmers, Let's build the Amazon store app </h1>
+      <Header />
       {/* Header */}
       {/* Home */}
     </div>
--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,50 @@ import './Header.css'
 
 function Header() {
     return (
+        // "header" is used to wrap all the elements present in the top most (header) part of the amazon app
         <div className='header'>
-            
+            {/* This is amazon logo */}
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+
+            {/* This is input search field */}
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                {/* Logo */}
+            </div>
+
+            {/* This is the three option placed in the Header */}
+            <div className="header__nav">
+                {/* This is the first option */}
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Hello Guests
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Sign in
+                    </span>
+                </div>
+
+
+                {/* This is the second option */}
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Returns
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Orders
+                    </span>
+                </div>
+
+                {/* This is the third option */}
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Your
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Prime
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
