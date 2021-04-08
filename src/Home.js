79aaaef3e049eb8fb9c51054f2da87e9ff44bc38
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-08 15:06:55 +0530

    product css: Style the product components.

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Product from './Product'
 
 function Home() {
     return (
@@ -9,8 +10,7 @@ function Home() {
                 <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.JPG" alt="" />
 
                 <div className="home__row">
-                    {/* Product */}
-                    {/* Product */}
+                    <Product />
                 </div>
 
                 <div className="home__row">
