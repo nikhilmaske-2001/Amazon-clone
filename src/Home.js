652dfd73848ace6818fc8faeb4c6b3570f7c165b
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-08 14:23:35 +0530

    home banner: Add a home banner in the img tag.
    Style the banner with mask-image to make it fade
    as it goes downward.

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,9 @@ import './Home.css'
 function Home() {
     return (
         <div className="home">
-            <p>I am Home</p>
+            <div className="home__container">
+                <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.JPG" alt="" />
+            </div>
         </div>
     )
 }
