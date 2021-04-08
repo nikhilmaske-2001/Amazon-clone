1c87427eb2f83cde13b83e5c3cc9480baf885fa2
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-08 14:33:02 +0530

    Structure: Make a new component as Product.
    Add a new files of js and css for Product components.
    Product will have the seaction under the banner section.
    There will be three sub-components in it.

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,23 @@ function Home() {
     return (
         <div className="home">
             <div className="home__container">
+                {/* img contains the banner just below the header */}
                 <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.JPG" alt="" />
+
+                <div className="home__row">
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+
+                <div className="home__row">
+                    {/* Product */}
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+
+                <div className="home__row">
+                    {/* Product */}
+                </div>
             </div>
         </div>
     )
