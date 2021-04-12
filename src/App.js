741dde648e4c0b9e289a9fb07af350b3f4763d3b
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-12 13:35:10 +0530

    npm router: Install router package to connect multiple pages to each other using switch.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
 import Header from './Header'
 import Home from './Home'
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     // We will use the BEM convention for naming
-    <div className="app">
-      <Header />
-      {/* Header */}
-      <Home />
-      {/* Home */}
-    </div>
+    // Router is used to sync more than one pages to each other
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            {/* Header */}
+            <h1>
+              I am a checkout page
+            </h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            {/* Header */}
+            <Home />
+            {/* Home */}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
