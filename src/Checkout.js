5333281c076ec4daa4398b7495267ce787236ef4
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-13 11:08:00 +0530

    checkout: Structured the checkout page with left and right side.

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,7 +3,21 @@ import "./Checkout.css";
 function Checkout() {
     return (
         <div className="checkout">
-
+            <div className="checkout__left">
+                <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
+                <div>
+                    <h2 className="checkout__title">Your Shopping Basket</h2>
+                    {/* All the basket items will go here */}
+                    {/* Basket */}
+                    {/* Basket */}
+                    {/* Basket */}
+                    {/* Basket */}
+                    {/* Basket */}
+                </div>
+            </div>
+            <div className="checkout__right">
+                <h2>Your subtotal will go here</h2>
+            </div>
         </div>
     )
 }
