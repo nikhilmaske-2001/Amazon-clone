ca42330cdb05e55cefcb3998fb633e1121cad788
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-13 17:34:01 +0530

    Subtotal: Complete the styling of the subtotal section.

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -5,7 +5,25 @@ import CurrencyFormat from "react-currency-format";
 function Subtotal() {
     return (
         <div className="subtotal">
-
+            <CurrencyFormat
+                renderText={(value) => (
+                    <>
+                        <p>
+                            {/* Part of the homework */}
+                            Subtotal (0 items): <strong>0</strong>
+                        </p>
+                        <small className="subtotal__gift">
+                            <input type="checkbox" />This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={0} //Part of the homework
+                displayType={"text"}
+                thousandSeperator={true}
+                prefix={"$"}
+            />
+            <button>Proceed to checkout</button>
         </div>
     )
 }
