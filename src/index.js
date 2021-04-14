ff7f96050743db7e7f51e14ca28b247409a355af
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-14 15:59:30 +0530

    Service worker not working.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { StateProvider } from './StateProvider';
 import reportWebVitals from './reportWebVitals';
-// import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,4 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-// serviceWorker.unregister();
+serviceWorker.unregister();
