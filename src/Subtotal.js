6b4212a042a32240d43014441410f4e29fcc42d3
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-15 18:24:42 +0530

    Subtotal: Pull the basket and import it correctly.

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import "./Subtotal.css"
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider"
 
 function Subtotal() {
+    const [{ basket }, dispatch] = useStateValue();
     return (
         <div className="subtotal">
             <CurrencyFormat
