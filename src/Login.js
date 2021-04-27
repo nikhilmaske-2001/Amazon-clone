635ae346544da0dca4dbc7eff751b99410c10abd
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-27 14:47:53 +0530

    All the changes in this commit does not work.

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
-import './Login.css'
-import { Link } from "react-router-dom"
+import React, { useState } from 'react';
+import './Login.css';
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
 
 function Login() {
+    const history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -15,6 +17,17 @@ function Login() {
     const register = e => {
         e.preventDefault();
 
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                //Succesfully created a new user with email and password
+                console.log(auth);
+                // if (auth) {
+                //     history.push('/');
+                // }
+            })
+            .catch(error => alert(error.messa
+            ))
         //firebase
     }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,19 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD7EKIXovtChwx_K_YH6wpGytm5lqvMkHw",
-    authDomain: "clone-ea57e.firebaseapp.com",
-    projectId: "clone-ea57e",
-    storageBucket: "clone-ea57e.appspot.com",
-    messagingSenderId: "93173779250",
-    appId: "1:93173779250:web:9cba5272c8474e3c57252b",
-    measurementId: "G-22F5C268TD"
-  };
+  apiKey: "AIzaSyD7EKIXovtChwx_K_YH6wpGytm5lqvMkHw",
+  authDomain: "clone-ea57e.firebaseapp.com",
+  projectId: "clone-ea57e",
+  storageBucket: "clone-ea57e.appspot.com",
+  messagingSenderId: "93173779250",
+  appId: "1:93173779250:web:9cba5272c8474e3c57252b",
+  measurementId: "G-22F5C268TD"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
